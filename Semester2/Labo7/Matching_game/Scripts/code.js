766bb7556f40155aa7a96218500bb60b34b11f15
r1046031 <ilona.defevere@student.vives.be>
2025-03-28 15:53:41 +0100

    commentaar toegevoegd

--- Semester2/Labo7/Matching_game/Scripts/code.js
+++ Semester2/Labo7/Matching_game/Scripts/code.js
@@ -1,8 +1,12 @@
 let global = {
+    //ik heb onderstaande global variables in commentaar gezet omdat ik ze niet heb gebruikt
     //AANTAL_HORIZONTAAL: 4,
     //AANTAL_VERTICAAL: 3,
     AANTAL_KAARTEN: 6,
     kaartjes: [[]],
+    //ik heb zelf een beetje mijn eigen uitbreidingen toegevoegd
+    //waaronder counter en aantalKliks. Counter houdt bij hoeveel kaartenreeksen de speler al heeft gevonden
+    //aantalKliks houdt bij hoeveel kaartjes de speler al heeft op geklikt
     counter: 0,
     timeout1: 0,
     timeout2: 0,
@@ -12,9 +16,11 @@ let global = {
 }
 
 const setup = () => {
+    //ik heb een button toegevoegd voor het spel dan nog eens opnieuw te spelen
     let btnReset = document.getElementById("btnReset");
     btnReset.addEventListener("click", reset)
 
+    //mijn array is een 2d array omdat ik zo bijhou hoeveel keer een type kaart al is omgedraaid
     global.kaartjes = [["bij",0], ["lieveheersbeestje",0], ["mier",0], ["rups",0], ["slak",0], ["worm",0]];
     console.log(global.kaartjes);
     kaartenToevoegen();
@@ -26,6 +32,7 @@ const kaartenToevoegen = () => {
     ["mier2", "mier grid-item"], ["rups2", "rups grid-item"], ["slak2", "slak grid-item"], ["worm2", "worm grid-item"]];
 
     //zodat ze iedere keer in een random volgorde op het "speelveld" terecht komen
+    //daarvan ook de array
     shuffle(afbeeldingen);
 
     for(let i = 0; i < afbeeldingen.length; i++) {
@@ -33,6 +40,7 @@ const kaartenToevoegen = () => {
     }
 }
 
+//shuffle is een functie die waarden in een array switcht
 const shuffle = (array) => {
     let huidigeIndex = array.length;
 
@@ -73,6 +81,8 @@ const kaartenVerwijderen = () => {
     img12.remove();
 }
 
+//om efficienter de images te creeëren heb ik nog een functie hiervoor gemaakt
+//anders veel copy-paste
 const createImage = (id, className, alt) => {
     let main = document.querySelector('main');
 
@@ -87,15 +97,17 @@ const createImage = (id, className, alt) => {
 }
 
 const omdraaien = (geklikt) => {
+    //if else
+    //de if else zorgt ervoor dat oftwel de voorkant of achterkant wordt getoond
+    //afhankelijk van welke kant er al wordt getoond
     if(geklikt.getAttribute('src') === (global.IMAGE_PATH_PREFIX + geklikt.classList[0] + global.IMAGE_PATH_SUFFIX)) {
         geklikt.setAttribute('src', (global.IMAGE_PATH_PREFIX + "achterkant" + global.IMAGE_PATH_SUFFIX));
         geklikt.style.pointerEvents = "auto";
 
+        //aantal keer dat de kaart is geklikt weer op 0 zetten
         let statusChange = false;
         let i = 0;
         while (statusChange === false && i < global.AANTAL_KAARTEN) {
-            //je kunt er nog op klikken, maar de image verandert niet door de 2d array die ik gebruik
-            //die bijhoudt hoeveel keer de kaart al is omgedraaid geweest
             if (global.kaartjes[i][0] === geklikt.classList[0]) {
                 global.kaartjes[i][1] = 0;
                 statusChange = true;
@@ -110,10 +122,10 @@ const omdraaien = (geklikt) => {
         let statusChange = false;
         let i = 0;
         while (statusChange === false && i < global.AANTAL_KAARTEN) {
-            //je kunt er nog op klikken, maar de image verandert niet door de 2d array die ik gebruik
-            //die bijhoudt hoeveel keer de kaart al is omgedraaid geweest
             if (global.kaartjes[i][0] === geklikt.classList[0]) {
                 if(global.kaartjes[i][1] !== 2) {
+                    //aantal keer dat de kaart is geklikt vermeerderen
+                    //met += 1 werkte het niet (ik weet niet waarom)
                     global.kaartjes[i][1] += 2;
 
                     geklikt.style.pointerEvents = "none";
@@ -122,8 +134,9 @@ const omdraaien = (geklikt) => {
                     console.log(global.kaartjes[i][1]);
 
                     //zodat als je een reeks images hebt gevonden, je nog even ze beide ziet voor ze weggaan
+                    //anders waren ze zo plotseling weg
                     clearTimeout(global.timeout1);
-                    global.timeout2 = setTimeout((event) => remove(geklikt), 1000);
+                    global.timeout2 = setTimeout((event) => hide(geklikt), 1000);
                 }
 
                 statusChange = true;
@@ -133,12 +146,14 @@ const omdraaien = (geklikt) => {
     }
 }
 
+//counter om te tellen hoeveel keer de speler al op een kaartje heeft geklikt
 const updateAantalKeerGeklikt = () => {
     let aantalKeerGeklikt = document.getElementById('aantalKliks');
     aantalKeerGeklikt.textContent = "Aantal keer geklikt: " + global.aantalKliks;
 }
 
-const remove = (geklikt) => {
+//voor een kaart te verstoppen
+const hide = (geklikt) => {
     let imagesMetClass = document.getElementsByClassName(geklikt.classList[0]);
     imagesMetClass[0].classList.add('hidden');
     imagesMetClass[1].classList.add('hidden');
@@ -146,13 +161,12 @@ const remove = (geklikt) => {
     counterUpdate();
 }
 
-//counter voor te tellen hoeveel de speler al juist heeft
+//counter voor te tellen hoeveel de speler al juist heeft updaten
 const counterUpdate = () => {
     let counterP = document.getElementById('counter');
     counterP.textContent = "Aantal kaartjes juist: " + global.counter + "/" + global.AANTAL_KAARTEN;
 }
 
-//ik heb een button toegevoegd voor het spel dan nog eens opnieuw te spelen
 const reset = () => {
     kaartenVerwijderen();
     kaartenToevoegen();
